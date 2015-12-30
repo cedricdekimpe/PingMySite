2820413e138ce67ed54740d918c06ce8041bb9ea
cedric <cedric@studio-sept.be>
2015-12-30 11:42:01 +0100

    First commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'dotenv-schema', '~> 0.0.1'
+gem 'thor', '~> 0.19.1'
+gem 'curb', '~> 0.9.0'
+gem 'slack-notifier', '~> 1.5', '>= 1.5.1'
---
+++ app.rb
@@ -0,0 +1,54 @@
+require "dotenv-schema"
+require "thor"
+require "curb"
+require "slack-notifier"
+
+Dotenv.load
+
+class PingMySite < Thor
+  option :follow_location, type: :boolean
+  option :expected_status_code, type: :numeric, required: true
+
+  desc "ping URL", "ping to URL"
+  def ping(url)
+    response_code = get_response_code(url)
+    if response_code.to_i == options[:expected_status_code]
+      # Everything's fine
+      puts "Sucessfully ping #{url} with expected status code #{options[:expected_status_code]}"
+    else
+      message = "Unable to ping #{url} with expected status code #{options[:expected_status_code]} - Received #{response_code} instead"
+      puts "Something went wrong, we must notify someone"
+      notify(message)
+    end
+  end
+
+  no_commands do
+
+    def get_response_code(url)
+      request(url).response_code
+    end
+
+    def request(url)
+      @request ||= Curl::Easy.new(url) do |curl|
+        curl.follow_location = options[:follow_location]
+      end
+      @request.perform
+      @request
+    end
+
+    def notify(message)
+      notifier = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
+      notifier.username = "Ping My Site"
+      notification = notifier.ping(message)
+      
+      if notification.response.code.to_i == 200
+        puts "Successfully notified to Slack"
+      else
+        puts "Something went wrong when trying to notify Slack"
+      end
+    end
+
+  end
+end
+
+PingMySite.start(ARGV)
